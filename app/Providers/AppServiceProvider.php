cbb7935eb2f5bb8cc5ba99210192eed2b3e88577
jrdeavila <morenojr15@hotmail.com>
2026-02-11 21:36:52 +0000

    Agregar redireccion https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,9 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
         Auth::provider('plaintext', function ($app, array $config) {
             return new PlainTextUserProvider();
         });
